afa626ab3be358824357ed694053bc1b6af66188
Camile Stefany <131724367+camsste@users.noreply.github.com>
2024-04-11 12:14:10 -0300

    Create atividade4.c

---
+++ atividade4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+float base, area, altura ;
+  printf(" Insira a base: ") ; 
+  scanf("%f", &base) ;
+  printf(" Insira a altura: ") ; 
+  scanf("%f", &altura) ;
+ area = altura * base ;
+  printf(" A sua área é: %2.f", area) ;
+}
