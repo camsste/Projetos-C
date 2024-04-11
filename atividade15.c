50e155f231c49f6ca9ec532eea08272c9bc729e0
Camile Stefany <131724367+camsste@users.noreply.github.com>
2024-04-11 12:21:33 -0300

    Create atividade15.c

---
+++ atividade15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  int cont = 0;
+  printf("Estrutura sequencial:\n\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("\n\n");
+  printf("Estrutura de repetição:\n\n");
+  while(cont<=11){
+    printf("Hello World\n");
+    cont++;
+  };
+  return 0;
+}
